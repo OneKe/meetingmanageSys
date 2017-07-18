afa37576190d212641b352ae5d102484e8c1c7e3
Administrator <Administrator@192.168.1.107>
2017-07-18 19:09:05 +0800

    saaf

--- src/com/chinasofti/mms/dao/MeetingParticipantsMapper.java
+++ src/com/chinasofti/mms/dao/MeetingParticipantsMapper.java
@@ -7,34 +7,17 @@ import com.chinasofti.mms.pojo.Meeting;
 import com.chinasofti.mms.pojo.MeetingParticipants;
 
 public interface MeetingParticipantsMapper {
-    /**
-     * @mbggenerated Mon Jul 17 14:35:17 CST 2017
-     */
+    
     int deleteByPrimaryKey(Integer mpid);
 
-    /**
-     * @mbggenerated Mon Jul 17 14:35:17 CST 2017
-     */
     int insert(MeetingParticipants record);
 
-    /**
-     * @mbggenerated Mon Jul 17 14:35:17 CST 2017
-     */
     int insertSelective(MeetingParticipants record);
 
-    /**
-     * @mbggenerated Mon Jul 17 14:35:17 CST 2017
-     */
     MeetingParticipants selectByPrimaryKey(Integer mpid);
 
-    /**
-     * @mbggenerated Mon Jul 17 14:35:17 CST 2017
-     */
     int updateByPrimaryKeySelective(MeetingParticipants record);
 
-    /**
-     * @mbggenerated Mon Jul 17 14:35:17 CST 2017
-     */
     int updateByPrimaryKey(MeetingParticipants record);
 
 	List<Meeting> selectByEmployeeName(Integer id);
