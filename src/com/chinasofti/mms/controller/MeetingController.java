f0c3e4bee24f5b536f5ce12ccfcd968e69fdda68
project <1940927916@qq.com>
2017-07-19 17:21:20 +0800

    yl BUG修改

--- src/com/chinasofti/mms/controller/MeetingController.java
+++ src/com/chinasofti/mms/controller/MeetingController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -63,10 +64,13 @@ public class MeetingController {
 	
 	@RequestMapping("/")
 	public ModelAndView MeetingDetail(HttpServletRequest request,HttpServletResponse response){
-		
-		
-		
 		return null;
 		
+	}
+	
+	public String selectMeeting(Model model){
+		List<Meeting> list=mservice.selectMeet();
+		model.addAttribute("list", list);
+		return null;
 	}
 }
